6e8797dc5f109fea210d83d86b167afe0ef46d1a
Shweta Sampatrao Kale <Shweta.Kale@asl.mum.sst>
2024-07-11 21:17:03 +0530

    Params Array and WriteLine uses params array

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArrayAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example of using a method with params array
+            DisplayNumbers(1, 2, 3, 4, 5);
+
+            // Example to demonstrate if WriteLine() uses params array internally
+            Console.WriteLine("Example of WriteLine() with params: {0}, {1}, {2}", 10, 20, 30);
+        }
+
+        static void DisplayNumbers(params int[] numbers)
+        {
+            Console.WriteLine("Numbers passed to DisplayNumbers method:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
